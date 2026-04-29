52027a57180edc083bb265788d218627133de03d
diego-rt <diego.terrones@imp.ac.at>
2026-04-29 23:34:56 +0200

    test mafft-profile for alignment merging

--- utils/RecursiveOldExt.sh
+++ utils/RecursiveOldExt.sh
@@ -511,10 +511,17 @@ while [ ${Family} -lt ${NumFamilies} ];do
                 --perc  ${percentVertical}
 
             #Merge alignment with previous rounds
-            mafft \
-                --seed ./Round_${CurrRound}/06_CurrentConsensi.${LowerCase}.Round${CurrRound}.Extended.tmp2.aln.fa \
-                --seed ./Round_${PrevRound}/07_CurrentConsensi.${LowerCase}.Round${PrevRound}.Extended.Curated.aln.fa \
-				/dev/null | \
+            #mafft \
+            #    --seed ./Round_${CurrRound}/06_CurrentConsensi.${LowerCase}.Round${CurrRound}.Extended.tmp2.aln.fa \
+            #    --seed ./Round_${PrevRound}/07_CurrentConsensi.${LowerCase}.Round${PrevRound}.Extended.Curated.aln.fa \
+			#	/dev/null | \
+            #seqkit grep -v -r -p "DUP" \
+            #> ./Round_${CurrRound}/06_CurrentConsensi.${LowerCase}.Round${CurrRound}.Extended.aln.fa
+
+            #Merge alignment with previous rounds
+            mafft-profile \
+                ./Round_${CurrRound}/06_CurrentConsensi.${LowerCase}.Round${CurrRound}.Extended.tmp2.aln.fa \
+                ./Round_${PrevRound}/07_CurrentConsensi.${LowerCase}.Round${PrevRound}.Extended.Curated.aln.fa | \
             seqkit grep -v -r -p "DUP" \
             > ./Round_${CurrRound}/06_CurrentConsensi.${LowerCase}.Round${CurrRound}.Extended.aln.fa
 
